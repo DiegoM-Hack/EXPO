6917d86f868e2c9c1719f1531ab5abd9bb862021
Diego Montaluisa <santitodiego12@gmail.com>
2025-05-01 00:12:41 -0500

    toques finales

--- Ejercicio3/src/Main.java
+++ Ejercicio3/src/Main.java
@@ -2,15 +2,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-
+        //Escaner
         Scanner sc = new Scanner(System.in);
         Jugador jugador1 =new Jugador("Messi", 10,"Delantero");
 
+        //Llamar al metodo
         jugador1.mostrar();
 
         jugador1.actualizarPosicion("Defensa");
         jugador1.mostrar();
 
+        //Actualizar por consola
         System.out.println("Ingrese la nueva posicion del jugador: ");
         String nuevaPosicion = sc.nextLine();
 
